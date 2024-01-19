e716877fe4b7653181839694f16d1c257c1610b8
Katarzyna <katarzyna.pabian357@gmail.com>
2024-01-19 21:34:08 +0100

    Polybius Square encryptation

--- PolybiusSquare.xaml.cs
+++ PolybiusSquare.xaml.cs
@@ -6,20 +6,14 @@ using System.Windows.Controls;
 
 namespace Bezpieczenstwo
 {
-    /// <summary>
-    /// Interaction logic for PolybiusSquare.xaml
-    /// </summary>
     public partial class PolybiusSquare : Window
     {
-        public char[] alphabet = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'w', 'x', 'y', 'z', 'ź', 'ż' };
+        public char[] alphabet = { 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'w', 'x', 'y', 'z', 'ź', 'ż', ' ' };
         Dictionary<char, string> cipher = new Dictionary<char, string>();
 
-        public PolybiusSquare()
-        {
-            InitializeComponent();
-        }
+        public PolybiusSquare() => InitializeComponent();
 
-        private void randButton_Click(object sender, RoutedEventArgs e)
+        private void RandButton_Click(object sender, RoutedEventArgs e)
         {
             char[] alph = alphabet;
             Random random = new Random();
@@ -38,39 +32,42 @@ namespace Bezpieczenstwo
             for (int i = 0; i < textboxList.Count; i++)
             {
                 if (i < alph.Length)
-                {
                     textboxList[i].Text = alph[i].ToString();
+            }
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<TextBox> textboxList = new List<TextBox> { S22, S23, S24, S25, S26, S32, S33, S34, S35, S36, S42, S43, S44, S45, S46, S52, S53, S54, S55, S56, S62, S63, S64, S65, S66, S72, S73, S74, S75, S76, S82, S83, S84, S85, S86, };
+
+                cipher.Clear();
+                for (int i = 0; alphabet.Length > i; i++)
+                {
+                    cipher.Add(alphabet[i], textboxList[i].Name);
                 }
+
+                StringBuilder newText = new StringBuilder();
+                foreach (char c in cipherText.Text)
+                {
+                    newText.Append(cipher[c]);
+                }
+
+                cipherText.Text = newText.ToString();
+            } catch (Exception ex)
+            {
+                throw ex;
+                // Jebać błędy
             }
         }
 
-        private void encryptButton_Click(object sender, RoutedEventArgs e)
-        {
-            List<TextBox> textboxList = new List<TextBox> { S22, S23, S24, S25, S26, S32, S33, S34, S35, S36, S42, S43, S44, S45, S46, S52, S53, S54, S55, S56, S62, S63, S64, S65, S66, S72, S73, S74, S75, S76, S82, S83, S84, S85, S86, };
-
-            for (int i = 0; alphabet.Length < i; i++)
-            {
-                cipher.Add(alphabet[i], textboxList[i].Name);
-            }
-
-
-
-            StringBuilder newText = new StringBuilder();
-            foreach (char c in cipherText.Text)
-            {
-                newText.Append(cipher[c]);
-            }
-
-            cipherText.Text = newText.ToString();
-
-        }
-
-        private void decipherButton_Click(object sender, RoutedEventArgs e)
+        private void DecipherButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void backButton_Click(object sender, RoutedEventArgs e)
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
